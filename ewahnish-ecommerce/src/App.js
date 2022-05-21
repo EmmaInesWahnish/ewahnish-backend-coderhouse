5aeea6ed7eb17bcca4f857179bdac57052654cf2
Emma Wahnish <ewahnish@gmail.com>
2022-05-21 17:37:13 -0300

    initial

---
+++ ewahnish-ecommerce/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Emma Ines Wawnish Backend Coder House
+      </header>
+    </div>
+  );
+}
+
+export default App;
