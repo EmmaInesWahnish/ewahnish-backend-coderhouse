ada92b197a0ed322b7bbb4e9062a74a0b51f2ba4
Emma Wahnish <ewahnish@gmail.com>
2022-06-14 19:47:52 -0300

    multer

--- ewahnish-ecommerce/src/js/expressServer.js
+++ ewahnish-ecommerce/src/js/expressServer.js
@@ -1,39 +1,50 @@
 import Products from "./Containers/productContainer.js";
 import productContainerLoad from "./productContainerLoadExpress.js"
 import express from 'express';
+import fs from 'fs'
 
 //const express = require("express");
 const app = express();
 const routerProducts = express.Router();
 
+// '/api/productos is our route for routerProducts
 app.use('/api/productos', routerProducts);
 
+// necessary for express to understand json format
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 routerProducts.use(express.json());
 routerProducts.use(express.urlencoded({ extended: true }))
 
-app.use('/static', express.static('public'))
+// product input form available at '/'
+app.use('/', express.static('public'))
 
 //Load products
 productContainerLoad();
-
-const PORT = 8080;
-
-const server = app.listen(PORT, () => {
-    console.log(`Server http listening at port ${server.address().port}`)
-})
-server.on("error", error => console.log(`Server error ${error}`))
 
 // Routes
 routerProducts.get('/', async (req, res) => {
     try {
         const array = await Products.getAll();
-        res.send({ message: 'Product List', products: array });
+        res.send({ message: 'Lista de productos ', products: array });
     }
     catch (error) {
-        console.log("An undetermined error has occured", error);
+        res.send({
+            message: 'No se ha podido recuperar la lista de productos'
+        })
+    }
+})
+
+routerProducts.get('/randomProduct', async (req, res) => {
+    try {
+        const array = await Products.getRandomProduct();
+        res.send({ mensaje: 'Un producto al azar ', products: array });
+    }
+    catch (error) {
+        res.send({
+            message: 'No se ha podido recuperar ningun producto'
+        })
     }
 })
 
@@ -53,23 +64,13 @@ routerProducts.get('/:id', async (req, res) => {
         }
         catch (error) {
             res.send({
-                "error": "Se produjo un error indeterminado al buscar el producto"
+                message: "Se produjo un error indeterminado al buscar el producto"
             })
         }
     } else {
         res.send({
             "error": "El id no es numerico"
         })
-    }
-})
-
-routerProducts.get('/randomProduct', async (req, res) => {
-    try {
-        const array = await Products.getRandomProduct();
-        res.send({ mensaje: 'Product List', products: array });
-    }
-    catch (error) {
-        console.log("File products.txt does not exist or is damaged", error);
     }
 })
 
@@ -80,71 +81,98 @@ routerProducts.post('/', async (req, res) => {
         price: receive.price,
         thumbnail: receive.thumbnail
     }]
-    try {
-        const productId = await Products.save(producto);
-        console.log(productId)
+    if (producto.title !== null) {
         try {
-            const products = await Products.getAll();
-            res.send({
-                productos: products
-            })
+            const productId = await Products.save(producto);
+            console.log(productId)
+            try {
+                const products = await Products.getAll();
+                res.send({
+                    productos: products
+                })
+            }
+            catch (error) {
+                res.send({
+                    message: 'No se ha podido obtener la lista de productos'
+                })
+            }
         }
         catch (error) {
             res.send({
-                message: 'Ha ocurrido un error'
+                message: 'No se ha podido salvar el producto'
             })
         }
-
-    }
-    catch (error) {
-        console.log("An undetermined error has occured", error);
+    } else {
+        res.send({
+            message: "Datos erroneos"
+        })
     }
 })
 
 routerProducts.put('/:id', async (req, res) => {
     const id = parseInt(req.params.id);
     let receive = req.body;
-    console.log("The id ",id, "receive  ", receive)
+    let searchedProduct = {};
+    console.log("The id ", id, "receive  ", receive)
     try {
         const products = await Products.getAll();
         const index = products.findIndex(element => element.id === id);
-        const searchedProduct = products[index]
-        console.log(index, " Modifico ",searchedProduct)       
-        if (searchedProduct != undefined) {
+        searchedProduct = products[index];
+        console.log(index, " Modifico ", searchedProduct);
+        if (index !== -1) {
 
-            products[index].title = receive.title;
-            products[index].price = receive.price;
-            products[index].thumbnail = receive.thumbnail;
+            if(receive.title !== null){
+                products[index].title = receive.title;
+            }
+            if(receive.price !== null){
+                products[index].price = receive.price;
+            }
+            if(receive.title !== null){
+                products[index].thumbnail = receive.thumbnail;
+            }
 
-            console.log("deberia modificar ", products[index])
+            let array = [];
+
+            products.forEach((element) => {
+                array.push({
+                    title: element.title,
+                    price: element.price,
+                    thumbnail: element.thumbnail
+                })
+            })
+
             try {
-                await Products.save([]);
+                await fs.promises.unlink('./src/files/products.txt');
                 try {
-                    await Products.save(products)
+                    await Products.save(array);
                     res.send({
-                        message: 'Modificado'
-                    })    
+                        message: 'Modificacion exitosa',
+                        product: array
+                    })
                 }
-                catch(error){
+                catch (error) {
                     res.send({
-                        message: 'No pudo cargar'
-                    })    
+                        message: 'No pudo cargar',
+                        error: error
+                    })
                 }
             }
             catch (error) {
                 res.send({
-                    message: 'No pudo borrar'
+                    message: 'No pudo borrar',
+                    error: error
                 })
             }
         } else {
             res.send({
-                message: 'No existe el producto'
+                message: 'Producto no encontrado'
             })
         }
     }
     catch (error) {
         res.send({
-            message: 'Ha ocurrido un error'
+            message: 'Ha ocurrido un error',
+            error: error
         })
     }
 
@@ -177,3 +205,10 @@ routerProducts.delete('/:id', async (req, res) => {
         })
     }
 })
+
+/* Server Listen */
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log(`Server http listening at port ${server.address().port}`)
+})
+server.on('error', error => console.log(`Error en servidor ${error}`))
